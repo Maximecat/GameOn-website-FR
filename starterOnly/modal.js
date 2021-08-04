cca76a8306e3045b3a37a3dc71cab967c7d5d76c
Maximecat <maximecat@outlook.fr>
2021-08-04 12:31:24 +0200

    modal.js

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -12,8 +12,11 @@ const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
 const closeBtn = document.querySelectorAll(".close");
+const firstInput = document.querySelector("#first");
+const lastInput = document.querySelector("#last");
+const emailInput = document.querySelector("#email");
 
-// launch modal event
+// Launch modal events
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 closeBtn.forEach((btn) => btn.addEventListener("click", closeModal));
 
@@ -22,34 +25,71 @@ function launchModal() {
   modalbg.style.display = "block";
 }
 
+// Fermeture de la modal
 function closeModal() {
   modalbg.style.display = "none";
 }
 
-function validate() {
-  const firstInput = document.querySelector("#first");
-  const lastInput = document.querySelector("#last");
-  const emailInput = document.querySelector("#email");
-  const birthdateInput = document.querySelector("#birthdate");
-  const quantityInput = document.querySelector("#quantity");
-
-  const firstValue = firstInput.value;
-  if(firstValue || firstValue.length < 2) {
-    firstInput.parentElement.setAttribute("data-error-visible", true);
-    firstInput.parentElement.setAttribute("data-error", "Le prénom est obligatoire et doit contenir 2 caractéres minimum");
+// Configuration des clefs pour les différent Inputs
+const inputConf = {
+  // Paramétres de la clef pour le Prénom
+  first: {
+    error:"Vous devez ecrire un prénom avec 2 caractéres minimum",
+    regExp:/^[a-zA-Z\-éëàèìòùÀÈÌÒÙáéíóúýÁÉÍÓÚÝâêîôûÂÊÎÔÛãñõÃÑÕäëïöüÿÄËÏÖÜŸçÇÆæœ]{2,}$/
+  },
+  // Paramétres de la clef pour le Nom
+  last: {
+    error:"Vous devez ecrire un nom avec 2 caractéres minimum",
+    regExp:/^[a-zA-Z\-éëàèìòùÀÈÌÒÙáéíóúýÁÉÍÓÚÝâêîôûÂÊÎÔÛãñõÃÑÕäëïöüÿÄËÏÖÜŸçÇÆæœ]{2,}$/
+  },
+  // Paramétres de la clef pour l'Email
+  email: {
+    error:"Vous devez saisir un email valide",
+    regExp:/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
   }
+};
 
-  const lastValue = lastInput.value;
-  if(lastValue || lastValue.length < 2) {
-    lastInput.parentElement.setAttribute("data-error-visible", true);
-    lastInput.parentElement.setAttribute("data-error", "Le nom est obligatoire et doit contenir 2 caractéres minimum");
+// Ecoute la modification du Prénom
+firstInput.addEventListener('change', function(event){
+  validateByRegExp(event, inputConf.first);
+});
+// Ecoute la modification du Nom
+lastInput.addEventListener('change', function(event){
+  validateByRegExp(event, inputConf.last)
+});
+// Ecoute la modification de l'Email
+emailInput.addEventListener('change', function(event){
+  validateByRegExp(event, inputConf.email)
+});
+
+// Validation d'un input donné en paramètre dans "event",
+// selon la configuration donnée en paramètre dans "conf"
+function validateByRegExp(event, conf) {
+  // Recupération de la valeur de l'input
+  const value = event.srcElement.value;
+
+  // Test de la valeur selon la configuration donnée
+  let testValue = conf.regExp.test(value);
+
+  // Résultat du test des regExp pour les inputs
+  if (testValue) {
+    // Applique "valid",l'element sera donc entouré par du vert
+    event.srcElement.parentElement.setAttribute("valid", true);
+    // Supprime "data-error-visible", car il n'y a pas d'erreur dans ce cas
+    event.srcElement.parentElement.removeAttribute("data-error-visible");
+    // Supprime "data-error", car il n'y a pas d'erreur dans ce cas 
+    event.srcElement.parentElement.removeAttribute("data-error");
+  } else {
+    // Supprime l'attribut "valid", car il y a une erreur
+    event.srcElement.parentElement.removeAttribute("valid");
+    // Applique "data-error-visible",l'element sera donc entouré par du rouge
+    event.srcElement.parentElement.setAttribute("data-error-visible", true);
+    // On ajoute le message d'erreur correspondant a l'input testé
+    event.srcElement.parentElement.setAttribute("data-error", conf.error);
   }
-
-  const emailValue = emailInput.value;
-  const birthdateValue = birthdateInput.value;
-  const quantityValue = (quantityInput.value);
-
-  console.log(firstValue, lastValue, emailValue, birthdateValue, quantityValue);
-  console.log("Fin");
-  
 }
+
+// Validation du formulaire complet
+function validate() {
+  console.log("validation de tout le formulaire");
+};
