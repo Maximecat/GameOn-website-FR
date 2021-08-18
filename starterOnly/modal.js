afe0b7e85f7ef47f44f526a3bfd0938a8ebf9308
Maximecat <maximecat@outlook.fr>
2021-08-18 11:27:26 +0200

    modal.js

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -35,13 +35,26 @@ function validate() {
   const firstInput = document.getElementById('first');
   const lastInput = document.getElementById('last');
   const emailInput = document.getElementById('email');
+  const birthdateInput = document.getElementById('birthdate');
+  const quantityInput = document.getElementById('quantity');
 
   let firstValue = firstInput.value;
   let lastValue = lastInput.value;
   let emailValue = emailInput.value;
+  let birthdateValue = birthdateInput.value;
+  let quantityValue = quantityInput.value;
 
-  console.log(firstValue, lastValue, emailValue);
+  let nameRegExp = /^[a-zA-Z\-éëàèìòùÀÈÌÒÙáéíóúýÁÉÍÓÚÝâêîôûÂÊÎÔÛãñõÃÑÕäëïöüÿÄËÏÖÜŸçÇÆæœ]{2,}$/;
+  let emailRegExp = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  let birthdateRegExp = /^(\d{4})(\/|-)(\d{1,2})(\/|-)(\d{1,2})$/
+  let quantityRegExp = /^\d$/
   
+  if (nameRegExp.test(firstValue) && nameRegExp.test(lastValue) && emailRegExp.test(emailValue) && birthdateRegExp.test(birthdateValue) && quantityRegExp.test(quantityValue)) {
+    console.log('ok');
+  } else {
+    console.error('pas ok')
+  }
+
   return false;
 
 };
