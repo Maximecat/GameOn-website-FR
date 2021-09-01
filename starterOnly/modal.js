19d5f60f366602cdd0c8545ff2dc318178a726b2
Maxime CAT <maximecat@outlook.fr>
2021-09-01 11:59:05 +0200

    Objet contenant la configuration des input : message d'erreur et regExp

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -29,35 +29,67 @@ function closeModal() {
   modalbg.style.display = "none";
 }
 
+// Objet contenant la configuration des input : message d'erreur et regExp
+const inputsConf = {
+  // Clef configuration pour le Prénom
+  first: {
+    error:"Le prénom doit contenir uniquement des lettres, 2 au minimum",
+    regExp: /^[a-zA-Z\-éëàèìòùÀÈÌÒÙáéíóúýÁÉÍÓÚÝâêîôûÂÊÎÔÛãñõÃÑÕäëïöüÿÄËÏÖÜŸçÇÆæœ]{2,}$/
+  },
+  // Clef configuration pour le Nom
+  last: {
+    error:"Le nom doit contenir uniquement des lettres, 2 au minimum",
+    regExp: /^[a-zA-Z\-éëàèìòùÀÈÌÒÙáéíóúýÁÉÍÓÚÝâêîôûÂÊÎÔÛãñõÃÑÕäëïöüÿÄËÏÖÜŸçÇÆæœ]{2,}$/
+  },
+  // Clef configuration pour l'Email
+  email: {
+    error:"Vous devez saisir un email valide, exemple : aaa@gmail.bb",
+    regExp: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+  },
+  // Clef configuration pour la date de naissance
+  birthdate: {
+    error:"Vous devez entrer vôtre date de naissance",
+    regExp: /^(\d{4})(\/|-)(\d{1,2})(\/|-)(\d{1,2})$/
+  },
+  // Clef configuration pour le nombre de participations
+  quantity: {
+    error:"Ce champ doit contenir un nombre de participations",
+    regExp: /^\d$/
+  },
+  radio: {
+    error:"Veuillez sélectionné une ville",
+    regExp : null
+  },
+  checkbox: {
+    error:"Vous devez accepté les conditions général d'utilisation",
+    regExp: null
+  }
+};
+
+const firstInput = document.getElementById('first');
+const lastInput = document.getElementById('last');
+const emailInput = document.getElementById('email');
+const birthdateInput = document.getElementById('birthdate');
+const quantityInput = document.getElementById('quantity');
+const radioInput = document.getElementsByName('location');
+const radioGroup = document.getElementById('radiogroup');
+const firstCheckboxInput = document.getElementById('checkbox1');
+
 // Validation du formulaire complet
 function validate() {
 
-  const firstInput = document.getElementById('first');
-  const lastInput = document.getElementById('last');
-  const emailInput = document.getElementById('email');
-  const birthdateInput = document.getElementById('birthdate');
-  const quantityInput = document.getElementById('quantity');
-  const radioInput = document.getElementsByName('location');
-  const radioGroup = document.getElementById('radiogroup');
-  const firstCheckboxInput = document.getElementById('checkbox1');
-
-  let firstValue = firstInput.value;
-  let lastValue = lastInput.value;
-  let emailValue = emailInput.value;
-  let birthdateValue = birthdateInput.value;
-  let quantityValue = quantityInput.value;
+  const firstValue = firstInput.value;
+  const lastValue = lastInput.value;
+  const emailValue = emailInput.value;
+  const birthdateValue = birthdateInput.value;
+  const quantityValue = quantityInput.value;
   let radioValue = null;
 
-  let nameRegExp = /^[a-zA-Z\-éëàèìòùÀÈÌÒÙáéíóúýÁÉÍÓÚÝâêîôûÂÊÎÔÛãñõÃÑÕäëïöüÿÄËÏÖÜŸçÇÆæœ]{2,}$/;
-  let emailRegExp = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  let birthdateRegExp = /^(\d{4})(\/|-)(\d{1,2})(\/|-)(\d{1,2})$/
-  let quantityRegExp = /^\d$/
-
-  const isFirstValid = nameRegExp.test(firstValue);
-  const isLastValid = nameRegExp.test(lastValue);
-  const isEmailValid = emailRegExp.test(emailValue);
-  const isBirthdateValid = birthdateRegExp.test(birthdateValue);
-  const isQuantityValid = quantityRegExp.test(quantityValue);
+  const isFirstValid = inputsConf.first.regExp.test(firstValue);
+  const isLastValid = inputsConf.last.regExp.test(lastValue);
+  const isEmailValid = inputsConf.email.regExp.test(emailValue);
+  const isBirthdateValid = inputsConf.birthdate.regExp.test(birthdateValue);
+  const isQuantityValid = inputsConf.quantity.regExp.test(quantityValue);
   const isFirstChecked = firstCheckboxInput.checked;
 
   for (var i = 0; i < radioInput.length; i++) {
@@ -67,49 +99,20 @@ function validate() {
       break;
     }
   };
-
-  const errorMessage = {
-    // Clef message d'erreur pour le Prénom
-    first: {
-      error:"Le prénom doit contenir uniquement des lettres, 2 au minimum"
-    },
-    // Clef message d'erreur pour le Nom
-    last: {
-      error:"Le nom doit contenir uniquement des lettres, 2 au minimum"
-    },
-    // Clef message d'erreur pour l'Email
-    email: {
-      error:"Vous devez saisir un email valide, exemple : aaa@gmail.bb"
-    },
-    // Clef message d'erreur pour la date de naissance
-    birthdate: {
-      error:"Vous devez entrer vôtre date de naissance"
-    },
-    // Clef message d'erreur pour le nombre de participations
-    quantity: {
-      error:"Ce champ doit contenir un nombre de participations"
-    },
-    radio: {
-      error:"Veuillez sélectionné une ville"
-    },
-    checkbox: {
-      error:"Vous devez accepté les conditions général d'utilisation"
-    }
-  };
   
-  validateInput(isFirstValid, firstInput, errorMessage.first.error);
+  validateInput(isFirstValid, firstInput, inputsConf.first.error);
 
-  validateInput(isLastValid, lastInput, errorMessage.last.error);
+  validateInput(isLastValid, lastInput, inputsConf.last.error);
 
-  validateInput(isEmailValid, emailInput, errorMessage.email.error);
+  validateInput(isEmailValid, emailInput, inputsConf.email.error);
 
-  validateInput(isBirthdateValid, birthdateInput, errorMessage.birthdate.error);
+  validateInput(isBirthdateValid, birthdateInput, inputsConf.birthdate.error);
 
-  validateInput(isQuantityValid, quantityInput, errorMessage.quantity.error);
+  validateInput(isQuantityValid, quantityInput, inputsConf.quantity.error);
 
-  validateInput(radioValue, radioGroup, errorMessage.radio.error);
+  validateInput(radioValue, radioGroup, inputsConf.radio.error);
 
-  validateInput(isFirstChecked, firstCheckboxInput, errorMessage.checkbox.error);
+  validateInput(isFirstChecked, firstCheckboxInput, inputsConf.checkbox.error);
 
 
   if(isFirstValid && isLastValid && isEmailValid && isBirthdateValid && isQuantityValid && isFirstChecked && radioValue) {
