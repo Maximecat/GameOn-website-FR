3d2ba45bce2b5e0b6fdc2f6586d6addf0dfeb84b
Maxime CAT <maximecat@outlook.fr>
2021-09-21 18:31:56 +0200

    Ajouts des commentaires

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -8,7 +8,7 @@ function editNav() {
 }
 
 /*
-* Récupération des éléments HTML / DOM pour la modal
+* Récupération des éléments HTML / DOM 
 */
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
@@ -18,65 +18,69 @@ const modalBody = document.querySelector(".modal-body");
 
 
 /*
-* Événements d'écoute pour le lancement et l'arrêt de la modal
+* Ajouts d'évènements au clique pour le lancement et la fermeture de la modal
 */
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 closeBtn.forEach((btn) => btn.addEventListener("click", closeModal));
 
 /*
-* Fonction lancement de la modal
+* Fonction de lancement de la modal
 */
 function launchModal() {
   modalbg.style.display = "block";
 }
 
 /*
-* Fonction fermeture de la modal
+* Fonction de fermeture de la modal
 */
 function closeModal() {
   modalbg.style.display = "none";
 }
 
 /*
-* Objet contenant la configuration des input : initialisé a false, lié au input associé,message d'erreur assosié et regExp
+* Objet contenant la configuration des inputs : 
+* - Initialisé a "false"
+* - Lié a l'input associé 
+* - Un message d'erreur assosié
+* - une regExp (Expression réguliére : décrit l'ensemble de chaînes de caractères possibles)
 */
 const inputsConf = {
-  /* Clef pour le Prénom et ses configurations */
+  /* Clef pour le Prénom */
   first: {
     isValid: false,
     input: document.getElementById('first'),
     error: "Le prénom doit contenir uniquement des lettres, 2 au minimum",
     regExp: /^[a-zA-Z\-éëàèìòùÀÈÌÒÙáéíóúýÁÉÍÓÚÝâêîôûÂÊÎÔÛãñõÃÑÕäëïöüÿÄËÏÖÜŸçÇÆæœ]{2,}$/
   },
-  /* Clef pour le Nom et ses configurations */
+  /* Clef pour le Nom */
   last: {
     isValid: false,
     input: document.getElementById('last'),
     error: "Le nom doit contenir uniquement des lettres, 2 au minimum",
     regExp: /^[a-zA-Z\-éëàèìòùÀÈÌÒÙáéíóúýÁÉÍÓÚÝâêîôûÂÊÎÔÛãñõÃÑÕäëïöüÿÄËÏÖÜŸçÇÆæœ]{2,}$/
   },
-  /* Clef pour l'Email et ses configurations */
+  /* Clef pour l'Email */
   email: {
     isValid: false,
     input: document.getElementById('email'),
     error: "Vous devez saisir un email valide, exemple : aaa@gmail.fr",
     regExp: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
   },
-  /* Clef pour la date de naissance et ses configurations */
+  /* Clef pour la Date de naissance */
   birthdate: {
     isValid: false,
     input: document.getElementById('birthdate'),
     error: "Vous devez entrer vôtre date de naissance",
     regExp: /^(\d{4})(\/|-)(\d{1,2})(\/|-)(\d{1,2})$/
   },
-  /* Clef pour le nombre de participation et ses configurations */
+  /* Clef pour le Nombre de participation */
   quantity: {
     isValid: false,
     input: document.getElementById('quantity'),
     error: "Ce champ doit contenir un nombre de participations",
     regExp: /^\d$/
   },
-  /* Clef pour les boutons radio et leurs configurations */
+  /* Clef pour les Boutons radio */
   radio: {
     isValid: false,
     inputs: document.getElementsByName('location'),
@@ -84,7 +88,7 @@ const inputsConf = {
     error: "Veuillez sélectionné une ville",
     regExp: null
   },
-  /* Clef pour la case a cocher et ses configurations */
+  /* Clef pour la case a cocher (CG) */
   checkbox: {
     isValid: false,
     input: document.getElementById('checkbox1'),
@@ -94,14 +98,14 @@ const inputsConf = {
 };
 
 /*
-* Validation du formulaire complet
+* Fonction pour la Validation du formulaire
 */
 function validate() {
-  //
+  // Pour la checkbox la valeur de 'isValid' passe a "true" si la case es cochée
   inputsConf.checkbox.isValid = inputsConf.checkbox.input.checked;
 
   /*
-  * Boucle pour les boutons radio, si un bouton et checked la boucle s'arréte et conserve la valeur coché
+  * Boucle pour les boutons radio, si un bouton et 'checked' la boucle s'arréte et conserve la valeur coché
   */
   for (var i = 0; i < inputsConf.radio.inputs.length; i++) {
     if (inputsConf.radio.inputs[i].checked) {
@@ -111,7 +115,10 @@ function validate() {
   };
 
   /*
-  * Pour toutes les clefs dans notre objet inputsConf si on peut testé une regExp sur notre valeur, faire appel a la fonction displayIsValidInput
+  * Pour toutes les clefs dans notre objet inputsConf,
+  * Si les clefs contiennent une regExp, 
+  * Changement de la valeur "false" de 'isValid' par la nouvelle valeur inscrite dans le champ, soumis au test de la regExp assosié.
+  * Faire appel a la fonction displayIsValidInput, permettant d'afficher l'erreur ou la validation sur l'input.
   */
   for (const key in inputsConf) {
     if (inputsConf[key].regExp) {
@@ -120,14 +127,9 @@ function validate() {
     displayIsValidInput(inputsConf[key].isValid, inputsConf[key].input, inputsConf[key].error);
   }
 
-
-
   /*
-  * Si les valeur de tout les inputs corresponde aux conditions et retourne 'true',
-  * Création d'une <div> avec une string de remerciement,
-  * Application d'un style a cette <div> en reprenant la hauteur de base de la modal,
-  * Récupération du bouton "Fermer" crée dans cette même div,
-  * Application d'une écoute pour la fermeture au clique du bouton "Fermer"
+  * Si toutes les valeurs des inputs correspondent aux conditions(regExp,checked), donc retourne 'true',
+  * Faire appel a la fonction displayThanks (Remerciement).
   */
   if (
     inputsConf.first.isValid &&
@@ -147,9 +149,9 @@ function validate() {
 };
 
 /*
-* Fonction pour validé ou non un input, 
-* Si isValid es "false", encadré rouge et message d'erreur,
-* Si isValid es "true", disparition du messsage d'erreur et encadré vert
+* Fonction pour afficher sur l'input l'erreur ou la validation du champ, 
+* Si isValid retourne "false", affiche un encadré rouge et message d'erreur,
+* Si isValid retourne "true", disparition de l'encadré rouge et du messsage d'erreur, pour un encadré vert.
 */
 function displayIsValidInput(isValid, inputElement, errorMessage) {
   if (!isValid) {
@@ -163,6 +165,14 @@ function displayIsValidInput(isValid, inputElement, errorMessage) {
   }
 }
 
+/*
+* Fonction pour afficher une fenêtre de remerciement,
+* Récupération de la taille de la modal de base,
+* Création de la modal de remerciement avec son message et un bouton "Fermer",
+* Application de la taille de la modal de base a celle de remerciement,
+* Récupération du bouton "Fermer",
+* Ajout d'un évènement de fermeture au clique sur le bouton.
+*/
 function displayThanks() {
   const bodyHeight = modalBody.offsetHeight;
 
